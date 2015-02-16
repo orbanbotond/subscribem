e32f3db819586f7259e3c0d5670edb923c9f0d72
Orban Botond <orbanbotond@gmail.com>
2015-02-16 22:18:51 +0200

    Correction is added

--- lib/subscribem/constraints/subdomain_required.rb
+++ lib/subscribem/constraints/subdomain_required.rb
@@ -2,7 +2,7 @@ module Subscribem
   module Constraints
     class SubdomainRequired
       def self.matches?(request)
-        request.subdomain.present? && request.subdomain != "www"
+        request.subdomain.present? && request.subdomain != "www" && request.subdomain != ENV['DEPLOYED_DOMAIN']
       end
     end
   end
--- lib/subscribem/version.rb
+++ lib/subscribem/version.rb
@@ -1,3 +1,3 @@
 module Subscribem
-  VERSION = "0.0.1"
+  VERSION = "0.0.002"
 end
