2051f1bbdbc080437b96a00eb94eb981ba3b591d
Orban Botond <orbanbotond@gmail.com>
2015-03-12 09:59:31 +0200

    The latest of slim and houser is used

--- app/extenders/controllers/application_controller_extender.rb
+++ app/extenders/controllers/application_controller_extender.rb
@@ -1,7 +1,7 @@
 ::ApplicationController.class_eval do
 
   def current_account
-    @current_account ||= env['X-Houser-Object']
+    @current_account ||= env['Houser-Object']
   end
 
   helper_method :current_account
--- lib/subscribem/version.rb
+++ lib/subscribem/version.rb
@@ -1,3 +1,3 @@
 module Subscribem
-  VERSION = '0.0.012'
+  VERSION = '0.0.013'
 end
